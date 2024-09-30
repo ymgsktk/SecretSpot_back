69ef46ae8ed60cc5a8204126125086f2c50e20b9
teruyukiKajiura <cy20253@shibaura-it.ac.jp>
2024-09-30 22:10:11 +0900

    ファイル名変更&属性変更

--- app/CandidateSpot.py
+++ app/CandidateSpot.py
@@ -1,17 +1,19 @@
-import Place
+import app.Spot as Spot
 from datetime import datetime
 
 # スポット候補地を表すクラス
-class PlaceOfCandidate(Place):
-    def __init__(self, name: str, address: str, latitude: str, longitude: str, estimated_arrival_time: datetime) -> None:
+class PlaceOfCandidate(Spot):
+    def __init__(self, name: str, address: str, latitude: str, longitude: str, estimated_arrival_time: datetime, evaluate: int) -> None:
         # 親クラス(Place)の属性を初期化
         super().__init__(name, address, latitude, longitude)
         
         self.estimated_arrival_time=estimated_arrival_time #到着予定時刻
+        self.evaluate=evaluate
+        #self.explanation=explanation
         
     def getEstimatedArrivalTime(self):
         return self.estimated_arrival_time
     
-    def serEstimatedArrivalTime(self,estimated_arrival_time: datetime):
+    def setEstimatedArrivalTime(self,estimated_arrival_time: datetime):
         self.estimated_arrival_time=estimated_arrival_time
     
